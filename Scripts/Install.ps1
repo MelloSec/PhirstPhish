bcf253b6cf636d44d98f0fe4fe71ab82b5c3e1f7
mellonaut <rleeazw@outlook.com>
2023-12-30 15:09:09 -0500

    sending working

--- Scripts/Install.ps1
+++ Scripts/Install.ps1
@@ -5,10 +5,6 @@ if (-not (Get-Module -Name AADInternals -ListAvailable)) {
     Write-Output "AADInternals module has been installed."
 }
 
-# Import the AADInternals module
-Import-Module AADInternals -Force
-Write-Output "AADInternals module has been imported."
-
 # Check if TokenTactics module is available
 if (!(Get-Module -ListAvailable -Name TokenTactics)) {
     # Download the latest TokenTactics zip if the module is not available
@@ -23,7 +19,3 @@ if (!(Get-Module -ListAvailable -Name TokenTactics)) {
     Get-ChildItem -Path .\tokentactics\tokentactics-main\* -Recurse | Move-Item -Destination .\tokentactics -Force
     Remove-Item -Path .\tokentactics\tokentactics-main -Recurse -Force
 }
-
-# Import or re-import the TokenTactics module
-Import-Module .\tokentactics\TokenTactics.psd1 -Force
-Write-Output "TokenTactics module has been imported."
